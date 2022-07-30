d6ff0aae9b71384ec4a9978fefb6aacf2f6b0498
Ketcap <7689820+Ketcap@users.noreply.github.com>
2022-07-30 15:44:32 +0200

    init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://uur.notion.site/Newsletter-8ca44ac1b49c4e399672f7777af74923',
+        permanent: true,
+      },
+      {
+        source:'/en',
+        destination:'https://uur.notion.site/Monthly-Newsletters-e3d3864d7a5c4bbfaca0cfa73eabb47c',
+        permanent:false
+      },
+      {
+        source:'/tr',
+        destination:'https://uur.notion.site/Ayl-k-Makaleler-1e3ccb22765341c781c768d736e68787',
+        permanent:false
+      }
+    ]
+  } 
 }
 
 module.exports = nextConfig
