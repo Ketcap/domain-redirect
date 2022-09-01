cc2aa656a6ef8acd8b93a976e0cbeaac63216912
Uğur Oruç <uoruc5@hotmail.com>
2022-09-01 18:03:49 +0200

    newsletters by month

--- next.config.js
+++ next.config.js
@@ -10,17 +10,33 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source:'/en',
-        destination:'https://uur.notion.site/Monthly-Newsletters-e3d3864d7a5c4bbfaca0cfa73eabb47c',
-        permanent:false
+        source: '/en',
+        destination: 'https://uur.notion.site/Monthly-Newsletters-e3d3864d7a5c4bbfaca0cfa73eabb47c',
+        permanent: false
       },
       {
-        source:'/tr',
-        destination:'https://uur.notion.site/Ayl-k-Makaleler-1e3ccb22765341c781c768d736e68787',
-        permanent:false
+        source: '/tr',
+        destination: 'https://uur.notion.site/Ayl-k-Makaleler-1e3ccb22765341c781c768d736e68787',
+        permanent: false
+      },
+      {
+        source: '/en/august-2022',
+        destination: 'https://uur.notion.site/August-2022-8308ec9131f7416ca2d28ca34972aa83',
+      },
+      {
+        source: '/tr/agustos-2022',
+        destination: 'https://uur.notion.site/A-ustos-2022-a4c0dd9bc02f479f9d21974de5e12152',
+      },
+      {
+        source: '/en/july-2022',
+        destination: 'https://uur.notion.site/July-2022-b5fdef8c740843d6bf989436efdd12cf',
+      },
+      {
+        source: '/tr/temmuz-2022',
+        destination: 'https://uur.notion.site/Temmuz-2022-cbf3fab8d2c0411096f053a8f36554fe',
       }
     ]
-  } 
+  }
 }
 
 module.exports = nextConfig
